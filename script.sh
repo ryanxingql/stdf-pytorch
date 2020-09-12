35c4274bc14da0688c2f153118f16fe0a4865a38
ryanxing <ryanxingql@gmail.com>
2020-09-13 02:31:16 +0800

    Hello world

--- script.sh
+++ script.sh
@@ -19,3 +19,5 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node
 # TEST
 # ==========
 
+# 1 GPU, no matter 1G, 2G or 4G. YAML for training and test should be the same!
+CUDA_VISIBLE_DEVICES=0 python test.py --opt_path option_R3_mfqev2_4G.yml
