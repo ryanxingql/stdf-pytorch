4342ae6751a4e7e460b3ec9b3772eaa5905841d3
ryanxing <ryanxingql@gmail.com>
2020-09-13 02:34:24 +0800

    Hello world

--- script.sh
+++ script.sh
@@ -10,10 +10,10 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=12345 train.py --opt_path option_R3_mfqev2_4G.yml
 
 # 2 GPUs
-#CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12354 train.py --opt_path option_R3_mfqev2_1G.yml
+#CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12354 train.py --opt_path option_R3_mfqev2_2G.yml
 
 # 1 GPU
-#CUDA_VISIBLE_DEVICES=0 python train.py --opt_path option_R3.yml
+#CUDA_VISIBLE_DEVICES=0 python train.py --opt_path option_R3_mfqev2_1G.yml
 
 # ==========
 # TEST
